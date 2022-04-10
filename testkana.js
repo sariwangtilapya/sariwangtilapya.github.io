ec32616b782b6cf0e23ffdd2a15bb84546873e25
sariwangtilapya <100106403+sariwangtilapya@users.noreply.github.com>
2022-04-10 17:06:35 +0800

    Update testkana.js

--- testkana.js
+++ testkana.js
@@ -1,7 +1,18 @@
-const hiragana=["あ", "い", "う", "え", "お", "か", "き", "く", "け", "こ", "さ", "し", "す", "せ", "そ", "た", "ち", "つ", "て", "と", "な", "に", "ぬ", "ね", "の", "は", "ひ", "ふ", "へ", "ほ", "ま", "み", "む", "め", "も", "ら", "り", "る", "れ", "ろ", "や", "ゆ", "よ", "わ", "を", "ん"]
+const hiragana=["あ", "い", "う", "え", "お", "か", "き", "く", "け", "こ", "さ", "し", "す", "せ", "そ", "た", "ち", "つ", "て", "と", "な", "に", "ぬ", "ね", "の", "は", "ひ", "ふ", "へ", "ほ", "ま", "み", "む", "め", "も", "や", "ゆ", "よ", "ら", "り", "る", "れ", "ろ", "わ", "を", "ん"]
 const romaji=["a" ,"i" ,"u" ,"e" ,"o" ,"ka" ,"ki" ,"ku" ,"ke" ,"ko" ,"sa" ,"shi" ,"su" ,"se" ,"so" ,"ta" ,"chi" ,"tsu" ,"te" ,"to" ,"na" ,"ni" ,"nu" ,"ne" ,"no" ,"ha" ,"hi" ,"fu" ,"he" ,"ho" ,"ma" ,"mi" ,"mu" ,"me" ,"mo" ,"ya" ,"yu" ,"yo" ,"ra" ,"ri" ,"ru" ,"re" ,"ro" ,"wa" ,"wo", "n"];
 
-let question=romaji; let réponse= hiragana;
+let question=[]; let réponse[];
+function PENA(limite){
+return Math.floor(Math.random()*limite);}
+
+function QeRConf(){
+for (i=0; i<5; i++){function randomiser(){
+let réponseAléa=PENA(hiragana.length);
+if (réponse.includes(hiragana[réponseAléa])){randomiser();}
+else {réponse.push(hiragana[réponseAléa]);
+question.push(romaji[réponseAléa]);}}
+randomiser();}} QeRConf();
+
 const résultatAff=document.getElementById("résultatHako")
 let questionUchi;
 
@@ -10,14 +21,20 @@ const boutonsAff=document.getElementById("boutonsHako");
 const questionAff=document.getElementById("questionHako");
 questionAff.innerText=question[questionUchi];
 
-function clicSuru(innerText){
-résultatDas=réponse.indexOf(innerText)===questionUchi?"🔴":"❌";
-résultatAff.innerText=résultatDas;
-questionUchi=Math.floor(Math.random()*question.length);
-questionAff.innerText=question[questionUchi];}
-
+function boutonsConf(){
 for (v of réponse){
 const réponseAff=document.createElement("button");
 réponseAff.innerText=v;
 réponseAff.setAttribute("onClick", "clicSuru(this.innerText)");
 boutonsAff.appendChild(réponseAff);}
+} boutonsConf();
+
+function clicSuru(innerText){
+résultatDas=réponse.indexOf(innerText)===questionUchi?"🔴":"❌";
+résultatAff.innerText=résultatDas;
+questionUchi=Math.floor(Math.random()*question.length);
+question=[]; réponse=[];
+ QeRConf();
+boutonsAff.innerHTML="";
+ boutonsConf();
+questionAff.innerText=question[questionUchi];}
