198f33c1a858167a140d8aa7fe59488c7d05e71f
sariwangtilapya <100106403+sariwangtilapya@users.noreply.github.com>
2022-04-10 14:35:37 +0800

    Update testkana.js

--- testkana.js
+++ testkana.js
@@ -1,6 +1,5 @@
-const hiragana=["あ","い","う","え","お"];
-
-const romaji=["a","i","u","e","o"];
+const hiragana=["あ", "い", "う", "え", "お", "か", "き", "く", "け", "こ", "さ", "し", "す", "せ", "そ", "た", "ち", "つ", "て", "と", "な", "に", "ぬ", "ね", "の", "は", "ひ", "ふ", "へ", "ほ", "ま", "み", "む", "め", "も", "ら", "り", "る", "れ", "ろ", "や", "ゆ", "よ", "わ", "を", "ん"]
+const romaji=["a" ,"i" ,"u" ,"e" ,"o" ,"ka" ,"ki" ,"ku" ,"ke" ,"ko" ,"sa" ,"shi" ,"su" ,"se" ,"so" ,"ta" ,"chi" ,"tsu" ,"te" ,"to" ,"na" ,"ni" ,"nu" ,"ne" ,"no" ,"ha" ,"hi" ,"fu" ,"he" ,"ho" ,"ma" ,"mi" ,"mu" ,"me" ,"mo" ,"ya" ,"yu" ,"yo" ,"ra" ,"ri" ,"ru" ,"re" ,"ro" ,"wa" ,"wo", "n"];
 
 let question=romaji; let réponse= hiragana;
 const résultatAff=document.createElement("p")
