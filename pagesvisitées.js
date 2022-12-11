1ff106b355538fbc531f4bd187ff33baa0044c99
sariwangtilapya <100106403+sariwangtilapya@users.noreply.github.com>
2022-12-12 02:16:55 +0800

    Update pagesvisitées.js

--- pagesvisitées.js
+++ pagesvisitées.js
@@ -1,5 +1,4 @@
         if (typeof(localStorage.getItem("visité"))!="string"){localStorage.setItem("visité", "0");}
-        if (localStorage.getItem("visité")==3){localStorage.setItem("visité", "0");}
+        if (localStorage.getItem("visité")=="3"){localStorage.setItem("visité", "0");}
         else {localStorage.setItem("visité", parseInt(localStorage.getItem("visité"))+1);}
-        document.body.innerText=localStorage.getItem("visité");
         console.log(localStorage.getItem("visité"));
