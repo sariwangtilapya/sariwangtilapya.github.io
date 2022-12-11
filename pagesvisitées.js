ce7350d0a59cdabdb234fdeff4444cbbcd7ce605
sariwangtilapya <100106403+sariwangtilapya@users.noreply.github.com>
2022-12-12 02:26:13 +0800

    Update pagesvisitées.js

--- pagesvisitées.js
+++ pagesvisitées.js
@@ -1,4 +1,4 @@
         if (typeof(localStorage.getItem("visité"))!="string"){localStorage.setItem("visité", "0");}
-        if (parseInt(localStorage.getItem("visité"))<2){localStorage.setItem("visité", "0");}
+        if (parseInt(localStorage.getItem("visité"))>2){localStorage.setItem("visité", "0");}
         else {localStorage.setItem("visité", parseInt(localStorage.getItem("visité"))+1);}
         console.log(localStorage.getItem("visité"));
