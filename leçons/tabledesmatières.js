a0e41893725dd87d3fee7273556f9799f7d77ae3
sariwangtilapya <100106403+sariwangtilapya@users.noreply.github.com>
2023-01-17 19:45:34 +0800

    Update tabledesmatières.js

--- leçons/tabledesmatières.js
+++ leçons/tabledesmatières.js
@@ -1,4 +1,4 @@
 tabledesmatières=["Phrase complète","Pronomesque","Particules de cas grammaticaux"];
 for (v of tabledesmatières){
-document.getElementByClass.innerHTML+=`<a href="${v}.html">${v}</a><br>`;
+document.getElementByClass("tab").innerHTML+=`<a href="${v}.html">${v}</a><br>`;
 }
