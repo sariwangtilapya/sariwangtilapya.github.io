8456db0c6e7de86adec86a102800c90c86296569
sariwangtilapya <sariwangtilapya@gmail.com>
2023-01-18 10:38:55 +0800

    suce ma bite

---
+++ en-tl/lessons/tabledesmatières.js
@@ -0,0 +1,4 @@
+const tabledesmatières=["adverb tails","all structures cheat sheet","declension table"];
+for (v of tabledesmatières){
+document.getElementById("tab").innerHTML+=`<a href="${v}.html">${v}</a><br>`;
+}
