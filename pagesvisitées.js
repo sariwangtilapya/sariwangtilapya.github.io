54c2f4914d7103348fa1d3cfb5b2acaffedbd5cc
sariwangtilapya <sariwangtilapya@gmail.com>
2023-01-18 08:59:48 +0800

    que dois-je dire alors?

--- pagesvisitées.js
+++
@@ -1,4 +0,0 @@
-        if (typeof(localStorage.getItem("visité"))!="string"){localStorage.setItem("visité", "0");}
-        if (localStorage.getItem("visité")=="3"){localStorage.setItem("visité", "0");}
-        else {localStorage.setItem("visité", parseInt(localStorage.getItem("visité"))+1)%4;}
-        console.log(localStorage.getItem("visité"));
