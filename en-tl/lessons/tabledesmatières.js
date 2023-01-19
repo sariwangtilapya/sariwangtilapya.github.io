f59e91d40ccd2ae22553a30850c3cc1ce3c68c16
sariwangtilapya <sariwangtilapya@gmail.com>
2023-01-19 11:46:04 +0800

    hjbjhb

--- en-tl/lessons/tabledesmatières.js
+++ en-tl/lessons/tabledesmatières.js
@@ -1,4 +1,4 @@
-const tabledesmatières=["adverb tails","all structures cheat sheet","declension table"];
+const tabledesmatières=["Imperative","Mayroon","all structures cheat sheet","declension table"];
 for (v of tabledesmatières){
 document.getElementById("tab").innerHTML+=`<a href="${v}.html">${v}</a><br>`;
 }
