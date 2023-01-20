4bc5b61484873e698ac321a9311a6530b3b5e99a
sariwangtilapya <sariwangtilapya@gmail.com>
2023-01-20 14:01:02 +0800

    bb

--- en-tl/lessons/tabledesmatières.js
+++ en-tl/lessons/tabledesmatières.js
@@ -1,4 +1,4 @@
-const tabledesmatières=["Imperative I","Infinitive","Mag vs Um"/*,"Mayroon"*/,"Enclitics","Sentence Structure"/*,"Ser vs Estar","declension table","all structure cheat sheet"*/];
+const tabledesmatières=["Imperative I","Infinitive","Mag vs Um","Imperative II","Repeated Expressions"/*,"Mayroon"*/,"Enclitics Overview","Enclitics I","Enclitics II","Enclitics III","Enclitics IV","Sentence Structure"/*,"Ser vs Estar","declension table","all structure cheat sheet"*/];
 for (v of tabledesmatières){
 document.getElementById("tab").innerHTML+=`<a href="${v}.html">${v}</a><br>`;
 }
