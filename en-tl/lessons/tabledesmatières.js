8c237a116ac73a075c1c8a35bd3c2d25bed7d372
sariwangtilapya <sariwangtilapya@gmail.com>
2023-01-23 01:41:21 +0800

    aqw

--- en-tl/lessons/tabledesmatières.js
+++ en-tl/lessons/tabledesmatières.js
@@ -1,4 +1,4 @@
-const tabledesmatières=["Parts of a Sentence","Epithet","Imperative I","Infinitive","Mag vs Um","Imperative II","Repeated Expressions"/*,"Mayroon"*/,"Enclitics Overview","Enclitics I","Enclitics II","Enclitics III","Enclitics IV","Sentence Structure"/*,"Ser vs Estar","declension table","all structure cheat sheet"*/];
+const tabledesmatières=["Parts of a Sentence","Attribute","Imperative I","Infinitive","Mag vs Um","Imperative II","Repeated Expressions"/*,"Mayroon"*/,"Enclitics Overview","Enclitics I","Enclitics II","Enclitics III","Enclitics IV","Sentence Structure"/*,"Ser vs Estar","declension table","all structure cheat sheet"*/];
 document.getElementById("tab").innerHTML=`<a href="Course Overview.html">Course Overview</a><br>`;
 for (v of tabledesmatières){
 document.getElementById("tab").innerHTML+=`<a href="${v}.html">${v}</a><br>`;
