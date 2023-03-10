3e911d0b67d51010da8fad770d1d95ede5d8822b
sariwangtilapya <sariwangtilapya@gmail.com>
2023-03-11 00:04:09 +0800

    aaa

--- fr-jp/Atelier de kanji/tabledesmatières.js
+++ fr-jp/Atelier de kanji/tabledesmatières.js
@@ -1,4 +1,4 @@
-const tabledesmatières=["ki moku","kuchi","royal","sous-particules","Mag vs Um","Enclitics I","Enclitics II","Enclitics III","Enclitics IV","Sentence Structure"/**/];
+const tabledesmatières=["ki moku","kuchi","royal","sous-particules"];
 document.getElementById("tab").innerHTML=`<a href="Course Overview.html">Course Overview</a><br>`;
 for (v of tabledesmatières){
 document.getElementById("tab").innerHTML+=`<a href="${v}.html">${v}</a><br>`;
