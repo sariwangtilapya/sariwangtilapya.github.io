c4474ab70d7adbec3dcf879e9ed6b8d6b3c17500
sariwangtilapya <sariwangtilapya@gmail.com>
2023-04-02 07:47:23 +0800

    nnn

---
+++ en-tl/grammar/navbar en-tl leçons.js
@@ -0,0 +1,18 @@
+document.getElementById("tête").innerHTML=`
+<nav id=navbar>
+    <div id=navbar-brand>
+        <h1>ＳＡＲＩＷＡＮＧ　ＴＩＬＡＰＹＡ</h1>
+    </div>
+    <div id=navbar-links>
+        <a href="/en-tl/home.html">Home</a></div></nav>
+    </div>
+</nav>
+
+<nav id=navbax>
+    <div id=navbar-brand>
+        <h1>ＳＡＲＩＷＡＮＧ　ＴＩＬＡＰＹＡ</h1>
+    </div>
+    <div id=navbar-links>
+        <a href="/en-tl/home.html">Home</a></div></nav>
+    </div>
+</nav>`;
---
+++ en-tl/grammar/tabledesmatières.js
@@ -0,0 +1,5 @@
+const tabledesmatières=["Notation","Vowel Mutations","Definiteness","Conjugation Table","Rules of Repetition I","Rules of Infixation","Rules of Prefixation","Rules of Suffixation","Nouns as Verbs","Parts of a Sentence","Attribute","Imperative I","Infinitive","Mag vs Um","Imperative II","Repeated Expressions"/*,"Mayroon"*/,"Enclitics Overview","Enclitics I","Enclitics II","Enclitics III","Enclitics IV","Sentence Structure"/*,"Ser vs Estar","declension table","all structure cheat sheet"*/];
+document.getElementById("tab").innerHTML=`<a href="Course Overview.html">Course Overview</a><br>`;
+for (v of tabledesmatières){
+document.getElementById("tab").innerHTML+=`<a href="${v}.html">${v}</a><br>`;
+}
