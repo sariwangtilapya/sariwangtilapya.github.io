986438d8e99d433cf6955b020f20efd546ac2681
sariwangtilapya <sariwangtilapya@gmail.com>
2023-04-12 19:33:49 +0800

    ,

--- en-tl/lessons/tabledesmatières.js
+++ en-tl/lessons/tabledesmatières.js
@@ -1,4 +1,4 @@
-const tabledesmatières=["Yes or No","Natural Phenomena","Verb Tenses","Verbs of Displacement","There is","Asking Where","Greeting"];
+const tabledesmatières=["Notation","Yes or No","Natural Phenomena","Verb Tenses","Verbs of Displacement","Adjectives","There is","Asking Where","Greeting","Chez moi"];
 document.getElementById("tab").innerHTML=`<a href="Course Overview.html">Course Overview</a><br>`;
 for (v of tabledesmatières){
 document.getElementById("tab").innerHTML+=`<a href="${v}.html">${v}</a><br>`;
