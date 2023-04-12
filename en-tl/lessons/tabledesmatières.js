d23329a6e3c721ab9fb39e82d9fc65d4adcd61fc
sariwangtilapya <sariwangtilapya@gmail.com>
2023-04-12 16:33:24 +0800

    ;

--- en-tl/lessons/tabledesmatières.js
+++ en-tl/lessons/tabledesmatières.js
@@ -1,4 +1,4 @@
-const tabledesmatières=["Yes or No","Natural Phenomena","Verb Tenses","Verbs of Displacement"/*,"Ser vs Estar","declension table","all structure cheat sheet"*/];
+const tabledesmatières=["Yes or No","Natural Phenomena","Verb Tenses","Verbs of Displacement","There is","Asking Where","Greeting"];
 document.getElementById("tab").innerHTML=`<a href="Course Overview.html">Course Overview</a><br>`;
 for (v of tabledesmatières){
 document.getElementById("tab").innerHTML+=`<a href="${v}.html">${v}</a><br>`;
