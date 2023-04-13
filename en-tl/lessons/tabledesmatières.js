56fd8cda91dee048cd1ee09eeba1134f77479717
sariwangtilapya <sariwangtilapya@gmail.com>
2023-04-13 18:32:25 +0800

    njk

--- en-tl/lessons/tabledesmatières.js
+++ en-tl/lessons/tabledesmatières.js
@@ -1,4 +1,4 @@
-const tabledesmatières=["Notation","Yes or No","Natural Phenomena","Verb Tenses","Verbs of Displacement","Adjectives","---brouillons ci-dessous---","There is","Asking Where","Chez moi"];
+const tabledesmatières=["Checklist","Notation","Yes or No","Natural Phenomena","Verb Tenses","Verbs of Displacement","Adjectives","---brouillons ci-dessous---","There is","Asking Where","Chez moi"];
 document.getElementById("tab").innerHTML=`<a href="Course Overview.html">Course Overview</a><br>`;
 for (v of tabledesmatières){
 document.getElementById("tab").innerHTML+=`<a href="${v}.html">${v}</a><br>`;
