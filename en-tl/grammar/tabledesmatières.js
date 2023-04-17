eb1b86aba93f24a0928c4d68c9b4a2b61b5ac5c4
sariwangtilapya <sariwangtilapya@gmail.com>
2023-04-17 18:30:37 +0800

    drt

--- en-tl/grammar/tabledesmatières.js
+++ en-tl/grammar/tabledesmatières.js
@@ -1,5 +1,5 @@
-const tabledesmatières=["Vowel Mutations","Definiteness","Conjugation Table","Hydration","Rules of Infixation","Rules of Prefixation","Rules of Suffixation","Nouns as Verbs","Parts of a Sentence","Epithet","Imperative I","Infinitive","Mag vs Um","Imperative II","Repeated Expressions","Mayroon","SPAM Overview"/*,"Enclitics I","Enclitics II","Enclitics III","Enclitics IV","Sentence Structure"/*,"Ser vs Estar","declension table","all structure cheat sheet"*/];
-document.getElementById("tab").innerHTML=`<a href="Course Overview.html">Course Overview</a><br>`;
+const tabledesmatières=["Sentence Structure","Main Predicate Unit","SPAM Overview","Declension Table","Conjugation Table","Rules of Prefixation","Rules of Hydration","Rules of Infixation","Rules of Suffixation","Definiteness","Malleable « ng »","Fused « ng »"/*,"Nouns as Verbs","Parts of a Sentence","Epithet","Repeated Expressions","Infinitive","Imperative II","Enclitics I","Enclitics II","Enclitics III","Enc,"Imperative I","Mag vs Um"litics IV","Ser vs Estar"*/];
+document.getElementById("tab").innerHTML=`GRAMMAR<br>`;
 for (v of tabledesmatières){
 document.getElementById("tab").innerHTML+=`<a href="${v}.html">${v}</a><br>`;
 }
