97bc9b65cf09086eead6c8d4d5eafc0eddc958e6
sariwangtilapya <sariwangtilapya@gmail.com>
2023-04-19 16:38:26 +0800

    vgh

--- con test.js
+++
@@ -1,2 +0,0 @@
-a="RTY";
-console.log(a);
--- en-tl/grammar/tabledesmatières.js
+++ en-tl/grammar/tabledesmatières.js
@@ -1,4 +1,4 @@
-const tabledesmatières=["Sentence Structure","Main Predicate Unit","Pronouns","SPAM Overview","Declension Table","Conjugation Table","Malleable « ng »","Fused « ng »","Rules of Hydration","Rules of Prefixation","Rules of Infixation","Rules of Suffixation","Definiteness","Numbers",/*,"Nouns as Verbs","Parts of a Sentence","Epithet","Repeated Expressions","Infinitive","Imperative II","Enclitics I","Enclitics II","Enclitics III","Enc,"Imperative I","Mag vs Um"litics IV","Ser vs Estar"*/];
+const tabledesmatières=["Sentence Structure","Main Predicate Unit","Declension Table","SPAM Overview","Rules of Hydration","Rules of Prefixation","Rules of Infixation","Rules of Suffixation","Conjugation Table","Definiteness","Numbers",/*,"Nouns as Verbs","Parts of a Sentence","Epithet","Repeated Expressions","Infinitive","Imperative II","Imperative I","Mag vs Um","Ser vs Estar"*/];
 document.getElementById("tab").innerHTML=`GRAMMAR --> Go to lessons <a href="../lessons/Checklist.html">here</a>.<br>`;
 for (v of tabledesmatières){
 document.getElementById("tab").innerHTML+=`<a href="${v}.html">${v}</a><br>`;
