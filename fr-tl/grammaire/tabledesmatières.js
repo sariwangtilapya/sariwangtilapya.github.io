e01b8cee167ad9a7c756f050844e4245882ff9ad
sariwangtilapya <sariwangtilapya@gmail.com>
2023-04-27 16:30:17 +0800

    vdskj

--- fr-tl/grammaire/tabledesmatières.js
+++ fr-tl/grammaire/tabledesmatières.js
@@ -1,4 +1,4 @@
-const tabledesmatières=["Sentence Structure","Main Predicate Unit","Declension Table","SPAM Overview","Rules of Hydration","Rules of Prefixation","Rules of Infixation","Rules of Suffixation","Pronoun »one«","Conjugations","Definiteness","Numbers",/*,"Nouns as Verbs","Parts of a Sentence","Epithet","Repeated Expressions","Infinitive","Imperative II","Imperative I","Mag vs Um","Ser vs Estar"*/];
+const tabledesmatières=["Structure de phrases","Groupe de prédicat principal","Tableau de déclination","Résumé de SPAMs","Règles of hydratation","Règles de préfixion","Règles d'infixion","Règles de suffixion","On","Conjugaisons","Definition","Nombres",/*,"Nouns as Verbs","Parts of a Sentence","Epithet","Repeated Expressions","Infinitive","Imperative II","Imperative I","Mag vs Um","Ser vs Estar"*/];
 document.getElementById("tab").innerHTML=`GRAMMAR --> Go to lessons <a href="../lessons/Checklist.html">here</a>.<br>`;
 for (v of tabledesmatières){
 document.getElementById("tab").innerHTML+=`<a href="${v}.html">${v}</a><br>`;
