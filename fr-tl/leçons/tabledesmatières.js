544a3e9d6a270a752b168a87a0f1c4b17e6641ad
sariwangtilapya <sariwangtilapya@gmail.com>
2023-04-27 12:48:58 +0800

    tyh

--- fr-tl/leçons/tabledesmatières.js
+++ fr-tl/leçons/tabledesmatières.js
@@ -1,5 +1,5 @@
-const tabledesmatières=["Checklist","Notation","Oui ou non","Phénomènes Naturels","Temps de verbes","Verbes de déplacement","Adjectifs","Prononciation de akó","Il y a"/*,"---drafts below---","na","pa","Asking Where","Chez moi"*/];
-document.getElementById("tab").innerHTML=`LEÇONS --> Aller à grammaire <a href="../grammaire/Structure de phrases.html">here</a>.<br>`;
+const tabledesmatières=["Checklist","Notation","Oui ou non","Phénomènes naturels","Temps de verbes","Verbes de déplacement","Adjectifs","Prononciation de akó","Il y a"/*,"---drafts below---","na","pa","Asking Where","Chez moi"*/];
+document.getElementById("tab").innerHTML=`LEÇONS --> Grammaire <a href="../grammaire/Structure de phrases.html">ici</a>.<br>`;
 for (v of tabledesmatières){
 document.getElementById("tab").innerHTML+=`<a href="${v}.html">${v}</a><br>`;
 }
