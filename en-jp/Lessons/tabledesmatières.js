59081479ca35e1366ceb5d349545dcf1cf871f7c
sariwangtilapya <sariwangtilapya@gmail.com>
2023-04-28 14:35:54 +0800

    bdhcjq

--- en-jp/Lessons/tabledesmatières.js
+++ en-jp/Lessons/tabledesmatières.js
@@ -1,4 +1,4 @@
-const tabledesmatières=["Complete Sentence","Predicates",""];
+const tabledesmatières=["Complete Sentence","Predicates","Predicates II","ある"];
 for (v of tabledesmatières){
 document.getElementById("tab").innerHTML+=`<a href="${v}.html">${v}</a><br>`;
 }
