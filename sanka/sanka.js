af88683f2b475104521614fe42b77c7de32fc811
sariwangtilapya <sariwangtilapya@gmail.com>
2023-05-23 19:36:35 +0800

    sdf

--- sanka/sanka.js
+++ sanka/sanka.js
@@ -11,8 +11,8 @@ if (localStorage.getItem("nvTilapya")==null){localStorage.setItem("nvTilapya",0)
 let nvDiff=nvDArt-parseInt(localStorage.getItem("nvTilapya"));
 if (nvDiff>0){document.getElementById("contenu").style.visibility="hidden";
 document.getElementById("pub").style.display="block";
-let tempsRestant=2**(2*nvDiff); délai(tempsRestant);
-if (nvDiff>3){document.getElementById("pub").style.display="block";
-document.getElementById("pub").innerHTML=`Il faut être au moins niveau ${nvDArt-3} pour accéder à cet article. Vous êtes à niveau ${localStorage.getItem("nvTilapya")}`;
+let tempsRestant=2**(nvDiff+1); délai(tempsRestant);
+if (nvDiff>5){document.getElementById("pub").style.display="block";
+document.getElementById("pub").innerHTML=`Il faut être au moins niveau ${nvDArt-5} pour accéder à cet article. Vous êtes à niveau ${localStorage.getItem("nvTilapya")}`;
 }
 }
