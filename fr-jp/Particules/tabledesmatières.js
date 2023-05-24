114f345d0507729a87c105a1e9f25511e9d5dc8d
sariwangtilapya <sariwangtilapya@gmail.com>
2023-05-24 21:43:05 +0800

    vwc

--- fr-jp/Particules/tabledesmatières.js
+++ fr-jp/Particules/tabledesmatières.js
@@ -2,9 +2,13 @@ const tabledesmatières=[];
 
 function redir(loc){document.location=`${loc}.html`}
 
-document.title="Particules";
+let d=0, f=0; pp=false; const dln=document.location.pathname, dll=dln.length;
+while(dln[dll-d]!="/"){if (dln[dll-d]=="."&&!pp){f=d;} d++;}
+const nomDeFiche=decodeURIComponent(dln.slice(dll-d+1,dll-f));
+
+document.title=nomDeFiche;
 document.getElementsByClassName("headTitre")[0].innerText="Particules";
-document.getElementsByClassName("headTitre")[1].innerText="Particules";
+// document.getElementsByClassName("headTitre")[1].innerText="Particules";
 
 fetch("tabledesmatières.txt")
 .then(response => response.text())
@@ -14,9 +18,10 @@ for (v of tabledesmatières){
 if (v[v.length-1]=="*"){//document.getElementById("tab").innerHTML+=`${v.slice(0,v.length-1)}<br>`;
     const ong=document.createElement("button"); ong.setAttribute("class","tablinks pasdispo");
     ong.innerText=v.slice(0,v.length-1); document.getElementById("tab").appendChild(ong);}
-else {//document.getElementById("tab").innerHTML+=`<a href="${v}.html">${v}</a><br>`;
-    const ong=document.createElement("button"); ong.setAttribute("class","tablinks");
-    ong.innerText=v; ong.setAttribute("onclick","redir(this.innerText)");
+else {const ong=document.createElement("button"); ong.innerText=v;
+if (v==nomDeFiche){ong.setAttribute("class","tablinks active");}
+else{ong.setAttribute("class","tablinks");}
+    ong.setAttribute("onclick","redir(this.innerText)");
     document.getElementById("tab").appendChild(ong)}
 }
 });
